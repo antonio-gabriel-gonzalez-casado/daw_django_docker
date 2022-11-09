563b96e36fe1ba5abdc61eaea0219f7537df1072
Antonio Gabriel González Casado <aggonzalezcasado@gmail.com>
2022-11-09 11:21:20 +0100

    Mejoras despliegue en test

--- app/entrypoint.test.sh
+++ app/entrypoint.test.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
 
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
-    done
-
-    echo "PostgreSQL started"
-fi
 
 # collect static files
 python manage.py collectstatic --noinput
