7ade5739427c45c49d25e825532afd75029478d5
Antonio Gabriel González Casado <aggonzalezcasado@gmail.com>
2022-11-09 11:48:50 +0100

    Fix settings.py

--- app/daw_django_docker/settings.py
+++ app/daw_django_docker/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-8@a$5rye%doa^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", default="localhost 127.0.0.1 [::1]").split(" ")
 
 
 # Application definition
